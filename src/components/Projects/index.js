df0cddd5eacdea9bd558131c91616b44a3b61842
maniaimam <manyaimam123@gmail.com>
2024-07-09 21:29:53 +0500

    changes in layout and design in projects and details page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import Hero from './Hero.js';
 import './ProjectCard.css';
 
 
 const ProjectsPage = ({ projects }) => {
   return (
     <div className="projects-page-container">
-     <Hero /> 
+      <h1 className='hero-title'>PROJECTS</h1>
       <div className="projects-page">
       {projects.map((project, index) => (
           <div key={index} className="project-card">
-            <img src={project.image} alt={project.title} className="project-image" />
+            <img src={project.mainImage} alt={project.title} className="project-image" />
             <div className="overlay">
               <div className="project-title">{project.title}</div>
               <a href={`/Projects/${project.id}`} className="project-link">View Project</a>
