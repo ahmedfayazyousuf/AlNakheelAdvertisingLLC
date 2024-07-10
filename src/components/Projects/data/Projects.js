66b1c0c94ea458238cb0b6dd26fe358acdca553f
mania imam <manyaimam123@gmail.com>
2024-07-11 01:44:17 +0500

    changes in projects details page

--- src/components/Projects/data/Projects.js
+++ src/components/Projects/data/Projects.js
@@ -5,16 +5,16 @@ const projects = [
       mainImage: "/images/Blogs/Blog1/MainImage.jpg",
       description: [
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/Blogs/Blog1/MainImage.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper.  ",
+          image: "/images/Blogs/Blog1/MainImage.jpg",
         },
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMG-2.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper.  ",
+          image: "/images/Blogs/Blog1/MainImage.jpg",
         },
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMGg-3.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper.  ",
+          image: "/images/Blogs/Blog1/img2.jpg",
         }
       ]
     },
@@ -24,16 +24,16 @@ const projects = [
       mainImage: "/images/Blogs/Blog1/MainImage.jpg",
       description: [
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMG-1.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper.  ",
+          image: "/images/IMG-1.jpg",
         },
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMG-2.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. ",
+          image: "/images/IMG-2.jpg",
         },
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMG-3.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. ",
+          image: "/images/IMG-3.jpg",
         }
       ]
     },
@@ -43,16 +43,16 @@ const projects = [
       mainImage: "/images/Blogs/Blog1/MainImage.jpg",
       description: [
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMG-1.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. ",
+          image: "/images/IMG-1.jpg",
         },
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMG-2.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. ",
+          image: "/images/IMG-2.jpg",
         },
         {
-          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur luctus dignissim orci quis tincidunt. ",
-          image: "/images/IMG-3.jpg"
+          paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non dapibus elit. In laoreet nec tortor id porttitor. In ipsum est, suscipit ac nisi ut, lacinia blandit dolor. Maecenas porta, enim ac ornare rutrum, velit ipsum porta turpis, non molestie odio est nec tellus. In in erat convallis, accumsan arcu vel, tristique nisl. Sed fringilla diam in condimentum congue. Morbi est orci, rutrum in viverra at, aliquam at augue. Nunc posuere nunc magna, sit amet congue sapien rutrum ut. Sed vitae massa eu mauris tristique ullamcorper.",
+          image: "/images/IMG-3.jpg",
         }
       ]
     }
