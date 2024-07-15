902edec2c6ac0b6eda569c7d6dd718a1bb77c4af
mania imam <manyaimam123@gmail.com>
2024-07-15 21:47:03 +0500

    fixed the ui and added layout of about page

--- src/components/MainPages/Home/Components/Intro/Intro.js
+++ src/components/MainPages/Home/Components/Intro/Intro.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import backdrop from '../../../../1_MediaAssets/BrandAssets/Backgrounds/backdrop2.jpg';
- import { useNavigate } from "react-router-dom";
+ //import { useNavigate } from "react-router-dom";
 
 const Intro = () => {
-   const navigate = useNavigate();
+   //const navigate = useNavigate();
   return (
     <div class="introduction" style={{backgroundImage: `url('${backdrop}')`, height: '100%', width: '100vw', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
       <div class="content">
         <div class="welcome-tag" style={{margin: '0'}}>AL NAKHEEL ADVERTISING LLC</div>
         <h1 className="hero-title outlined-text" style={{color: 'transparent', fontSize: '35px', margin: '0'}}>Your Vision, Our Expertise.</h1>
         <p style={{fontSize: '14px', color: 'white'}}>Empower Your Business with Strategic Advertising Solutions.</p>
-        { <button onClick= {()=> navigate('/About')} className='buttonDefault' style={{width: '120px', height: '50px', fontSize: '14px', marginTop: '10px'}}>View Projects</button> }
+        {/* <button onClick= {()=> navigate('/About')} className='buttonDefault' style={{width: '120px', height: '50px', fontSize: '14px', marginTop: '10px'}}>View Projects</button>*/ }
       </div>
     </div>
   );
